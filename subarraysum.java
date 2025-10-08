08ad820f4976a0bd7e8795ebe91b5b3bd9b853db
venkatesanweb <venkatesan.kumarsivan@gmail.com>
2025-10-08 20:16:22 +0530

    subarray normal methord

---
+++ subarraysum.java
@@ -0,0 +1,22 @@
+public class subarraysum {
+    public static void main(String[] args) {
+        int[] arr = {1,2,3};
+        int max=0;
+        for (int start = 0; start < arr.length; start++) {//0 ;1;2
+            for (int end = start; end < arr.length; end++) {//0 1,2;1,2;2
+                int sum = 0;
+                int count=0;
+                for (int k = start; k <= end; k++) {//0;0,1;0,1;0,1,2;;1;1,2;2;   ;;;1;1,2;1,2,3;;2;2,3;;3
+                    sum += arr[k];
+                    count++;
+                }
+                if(sum==3 && count>max){
+                    max=count;
+                }
+                // System.out.println("Subarray sum from " + start + " to " + end + " = " + sum);
+            }
+        }
+        System.out.println(max);
+
+    }
+}
