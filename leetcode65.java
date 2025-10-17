91773afc6970129efd54eed43bae7f9c050f6ef3
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2025-10-17 11:26:50 +0530

    Add files via upload
    leetcode65 hard problem completed

---
+++ leetcode65.java
@@ -0,0 +1,15 @@
+class leetcode65 {
+    public boolean isNumber(String s) {
+        try{
+            int l=s.length();
+            if(s.equals("Infinity") ||s.equals("-Infinity") ||s.equals("+Infinity") ||s.charAt(l-1)=='d'||s.charAt(l-1)=='f'||s.charAt(l-1)=='F'||s.charAt(l-1)=='D'){
+                return false;
+            }
+            double num=Double.parseDouble(s);
+            return true;
+        }
+        catch(Exception e){
+            return false;
+        }
+    }
+}
