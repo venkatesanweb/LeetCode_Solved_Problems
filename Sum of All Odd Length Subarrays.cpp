5242b14645b7a75e2a2ce4349d617e6bc399b283
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-03-19 20:26:51 +0530

    Create Sum of All Odd Length Subarrays.cpp

---
+++ Sum of All Odd Length Subarrays.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int sumOddLengthSubarrays(vector<int>& arr) {
+        int ans=0;
+        for(int i=0;i<arr.size();i++){
+            int sum=0;
+            for(int j=i;j<arr.size();j++){
+                sum+=arr[j];
+                if((j-i)%2==0){
+                    ans+=sum;
+                }
+            }
+        }
+        return ans;
+    }
+};
