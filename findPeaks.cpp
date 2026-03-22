a9295e4a94e03fc4b4915757cfbfcbcee93e6e8b
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-03-22 20:08:28 +0530

    Create findPeaks.cpp

---
+++ findPeaks.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    vector<int> findPeaks(vector<int>& mountain) {
+        vector<int>vc;
+        for(int i=1;i<mountain.size()-1;i++){
+            if(mountain[i]>mountain[i-1] && mountain[i]>mountain[i+1]) vc.push_back(i);
+        }
+        return vc;
+    }
+};
