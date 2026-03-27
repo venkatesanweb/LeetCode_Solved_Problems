9872f4a6b72784f5dfd3b18b877eac6ae663baad
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-03-27 22:11:36 +0530

    Create relativeSortArray.cpp

---
+++ relativeSortArray.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> relativeSortArray(vector<int>& arr1, vector<int>& arr2) {
+        int k=0;
+        for(int i=0;i<arr2.size();i++){
+            for(int j=0;j<arr1.size();j++){
+                if(arr2[i]==arr1[j]){
+                    swap(arr1[k++],arr1[j]);
+                }
+            }
+            
+        }
+        if(k<arr1.size()){
+            sort(arr1.begin()+k,arr1.end());
+        }
+        return arr1;
+    }
+};
