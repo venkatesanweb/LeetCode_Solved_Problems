71a051852835cbe6a0b383e61147dd4bf47b459e
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-03-30 16:23:28 +0530

    Create subtractProductAndSum.cpp

---
+++ subtractProductAndSum.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int subtractProductAndSum(int n) {
+        int pro=1;
+        int sum=0;
+        while(n>0){
+            int dig=n%10;
+            pro*=dig;
+            sum+=dig;
+            n/=10;
+        }
+        return pro-sum;
+        
+    }
+};
