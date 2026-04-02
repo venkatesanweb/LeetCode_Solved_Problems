a1dd307c29a15164dcdd92a804cbfaa127e54c6d
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-04-02 21:43:41 +0530

    Create reverseWords.cpp

---
+++ reverseWords.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    string reverseWords(string s) {
+        int j=0;
+        int n=s.size();
+        for(int i=0;i<=n;i++){
+            if(s[i]==' ' || i==n){
+                reverse(s.begin()+j,s.begin()+i);
+                j=i+1;
+            }
+        }
+        return s;
+    }
+};
