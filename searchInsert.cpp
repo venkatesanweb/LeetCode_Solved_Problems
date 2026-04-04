9fbeb88c308edd8cc99d4daf64362440d157f3e1
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-04-04 14:56:37 +0530

    Create searchInsert.cpp

---
+++ searchInsert.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int left=0;
+        int right=nums.size()-1;
+        int ans=nums.size();
+        while(left<=right){
+            int mid=left+(right-left)/2;
+            if(nums[mid]>=target){
+                ans=mid;
+                right=mid-1;
+            }
+            else{
+                left=mid+1;
+            }
+        }
+        return ans;
+    }
+};
