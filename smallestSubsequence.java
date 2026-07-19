3033754ef1da2da7b96315d212bb53f45ecc73a0
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-07-19 10:57:58 +0530

    Create smallestSubsequence.java

---
+++ smallestSubsequence.java
@@ -0,0 +1,52 @@
+import java.util.Stack;
+
+class Solution {
+    public String smallestSubsequence(String s) {
+        int[] freq = new int[27];
+        boolean[] seen = new boolean[27];
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++)
+            freq[s.charAt(i) & 31]++;
+
+        for (int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            int x = c & 31;
+            freq[x]--;
+
+            if (seen[x])
+                continue;
+
+            while (!stack.isEmpty()) {
+                if (stack.peek() <= c)
+                    break;
+
+                if (freq[stack.peek() & 31] == 0)
+                    break;
+
+                seen[stack.peek() & 31] = false;
+                stack.pop();
+            }
+
+            stack.push(c);
+            seen[x] = true;
+        }
+
+        StringBuilder res = new StringBuilder();
+
+        for (char c : stack)
+            res.append(c);
+
+        return res.toString();
+    }
+
+    public static void main(String[] args) {
+        Solution obj = new Solution();
+
+        String s = "bcabc";
+
+        String result = obj.smallestSubsequence(s);
+
+        System.out.println(result);
+    }
+}
