b70c8f1526ccf1e0a047af07e2635965e78a6891
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-07-27 13:20:12 +0530

    Create maxProduct3.java

---
+++ maxProduct3.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int maxProduct(int[] nums) {
+        int n = nums.length;
+        int max = 0;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = i + 1; j < n; j++) {
+                max = Math.max(
+                    (nums[i] - 1) * (nums[j] - 1),
+                    max
+                );
+            }
+        }
+
+        return max;
+    }
+
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+
+        int[] nums = {3, 4, 5, 2};
+
+        System.out.println(sol.maxProduct(nums));
+    }
+}
