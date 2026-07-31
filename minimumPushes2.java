8faac842f46cefee1dbf0b3ba11a651b59a651de
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-07-31 13:28:07 +0530

    Implement minimumPushes method for character counting

---
+++ minimumPushes2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+
+class Solution {
+
+    public int minimumPushes(String word) {
+        int[] hashTable = new int[26];
+
+        for (char ch : word.toCharArray()) {
+            hashTable[ch - 'a']++;
+        }
+
+        int minPushing = 0;
+
+        Arrays.sort(hashTable);
+
+        for (int i = 25, pushCnt = 0; i >= 0; i--) {
+            int currEleIdx = 25 - i;
+
+            if (currEleIdx % 8 == 0)
+                pushCnt++;
+
+            minPushing += hashTable[i] * pushCnt;
+        }
+
+        return minPushing;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String word = sc.next();
+
+        Solution sol = new Solution();
+        int result = sol.minimumPushes(word);
+
+        System.out.println(result);
+
+        sc.close();
+    }
+}
