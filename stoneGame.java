a5b7a09552f44c21b2aca303870fe5e5eca980f9
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-08-02 15:23:06 +0530

    Implement stoneGame method and main function

---
+++ stoneGame.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+
+    public static boolean stoneGame(int[] piles) {
+        return true;
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] piles = new int[n];
+
+        for (int i = 0; i < n; i++) {
+            piles[i] = sc.nextInt();
+        }
+
+        System.out.println(stoneGame(piles));
+    }
+}
