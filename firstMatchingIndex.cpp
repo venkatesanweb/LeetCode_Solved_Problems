41727e78e0cafa8167d67e1193d9a107d735f205
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-08-05 14:36:00 +0530

    Create firstMatchingIndex.cpp

---
+++ firstMatchingIndex.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int firstMatchingIndex(string s) {
+        int i=0;
+        int j=s.length()-1;
+        while(i<=j){
+            if(s[i]==s[j]) return i;
+            i++;
+            j--;
+        }
+        return -1;
+    }
+};
