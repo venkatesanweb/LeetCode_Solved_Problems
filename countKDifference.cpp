6b1d93d3febc0cca8cd37b1031affb3dafa1e0a1
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-08-07 07:21:04 +0530

    Create countKDifference.cpp

---
+++ countKDifference.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int countKDifference(vector<int>& nums, int k) {
+        int co =0;
+        for(int i=0;i<nums.size();i++){
+            for(int j=0;j<nums.size();j++){
+                if(abs(nums[i]-nums[j]==k)) co++;
+            }
+        }
+        return co;
+    }
+};
