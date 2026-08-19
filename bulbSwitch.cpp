0b68a0a5ea931a4de4ec046ad6cd67b58549048d
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-08-19 13:48:41 +0530

    Implement bulbSwitch function in Solution class

---
+++ bulbSwitch.cpp
@@ -0,0 +1,11 @@
+class Solution {
+    public int bulbSwitch(int n) {
+        long i=1;
+        int ct=0;
+        while((i*i)<=n){
+            i++;
+            ct++;
+        }
+        return ct;
+    }
+}
