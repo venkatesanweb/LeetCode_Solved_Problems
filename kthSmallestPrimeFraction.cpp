d71e4089cd9e22f0e6d42721f8bc40d6da5a4cfb
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-08-19 07:38:45 +0530

    Create kthSmallestPrimeFraction.cpp

---
+++ kthSmallestPrimeFraction.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> kthSmallestPrimeFraction(vector<int>& arr, int k) {
+        priority_queue<pair<float, pair<int, int>>> q;
+        int n=arr.size();
+        for(int i=0; i<n-1; i++){
+            for(int j=i+1; j<n; j++){
+               q.push({(float)arr[i]/arr[j],{arr[i], arr[j]}});
+                if(q.size() > k)q.pop();
+            }
+        }
+        return {q.top().second.first, q.top().second.second};
+    }
+};
