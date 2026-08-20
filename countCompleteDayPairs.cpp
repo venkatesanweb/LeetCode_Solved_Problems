d80ff6113f7c523c022db500320085b506d87438
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-08-20 22:04:20 +0530

    Create countCompleteDayPairs.cpp

---
+++ countCompleteDayPairs.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int countCompleteDayPairs(vector<int>& hours) {
+        int co=0;
+        int n= hours.size();
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                if((hours[i]+hours[j])%24==0) co++;
+            }
+        }
+        return co;
+    }
+};
