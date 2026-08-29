f73d273712944d76c9af6e791fc36877d55686ef
Venkatesan Kumar <venkatesan.kumarsivan@gmail.com>
2026-08-29 21:57:25 +0530

    Create Sleep.js

---
+++ Sleep.js
@@ -0,0 +1,15 @@
+/**
+ * @param {number} millis
+ * @return {Promise}
+ */
+async function sleep(millis) {
+    const P = await new Promise((resolve,reject)=>{
+        setTimeout(()=>resolve(millis),millis);
+    }); 
+    return P;
+}
+
+/** 
+ * let t = Date.now()
+ * sleep(100).then(() => console.log(Date.now() - t)) // 100
+ */
